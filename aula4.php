beaf3b69ae9d685e71a7b5510f2410ffbe5adf8a
BrunoLB2007 <brunolerin2007@gmail.com>
2024-07-16 21:50:42 -0300

    aula4.php

---
+++ aula4.php
@@ -0,0 +1,19 @@
+<?php 
+//faltou seu vagabudo//
+
+// calcular e eibir a tabuada de acordo com o numero informado pelo usuário//
+//utilizar a variavel de loops pg11//
+
+$numero =4;
+
+$i = 0;
+$i = $i +1; // pra esse ser possivel tem que inciar a variavel antes// 
+
+for ($i = 0 ; $i <=10; $i++) { //enquanto a variavel i for menor ou igual a zero o loop continua//
+   $resultado = $numero * $i;
+   echo $numero . "x" . $i . "=" . $resultado . "<br>";
+
+
+}
+
+// variavel de nome i sem motivo especifico, tipo index ou informação//
