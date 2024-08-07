b746f830e4016cd19d23b74351fc4b2d381076f0
BrunoLB2007 <brunolerin2007@gmail.com>
2024-08-06 22:02:36 -0300

    Create aula8.php

---
+++ aula8.php
@@ -0,0 +1,50 @@
+<?php 
+
+$alfa= ["a", "c", "b", "d"];
+$tamanhoalfa= count($alfa);
+$maxindice = $tamanhoalfa - 1;
+
+for ($i = 0; $i < $tamanhoalfa; $i++) {
+
+    $maxindice_2= $tamanhoalfa - $i - 1;
+
+    for ($j = 0; $j < $tamanhoalfa_2 -1; $j++) {
+
+        if ($alfa[$j] > $alfa[$j + 1]) {
+
+            // Troca os elementos para ordenar corretamente
+
+            $temp = $alfa[$j];
+            $alfa[$j] = $alfa[$j + 1];
+            $alfa[$j + 1] = $temp;
+        }
+     }
+}
+
+
+echo implode (", ", $alfa );
+
+
+
+
+/*
+Identificar o tipo de triangulo conforme os dados inseridos pelo usuário 
+sendo três medidas: $lado1, $lado2, $lado3
+A partir destas 3 medidas efetuar o calculo e informar qual o tipo de triangulo formado pelas medidas 
+
+podems ser isoceles, equilatero ou escaleno 
+*/
+
+$lado1 = 2;
+$lado2 = 1;
+$lado3 = 1;
+
+echo "<br>";
+
+if ( $lado1 == $lado2 && $lado2 == $lado3 ){
+    echo "triangulo equilátero";
+
+}elseif ($lado1 = $lado2| $lado1 = $lado3| $lado2 = $lado3){
+    echo "triangulo isóceles";
+}
+
