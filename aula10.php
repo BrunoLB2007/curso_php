5238f4eaa12b2ae8606a0b46670a742e49083b14
BrunoLB2007 <brunolerin2007@gmail.com>
2024-08-20 22:04:35 -0300

    aula10.php

---
+++ aula10.php
@@ -0,0 +1,18 @@
+<?php 
+
+/* construir funções (separadas) onde cada uma recee um parametro (string, texto)
+e retorna um valor boolean (True||False)
+
+objetivo: Validar um cpf e um cnpj 
+
+dica, nome da função começa com isValid
+
+isValidCPF isValidCNPJ
+
+existe uma formula para validar cpf e cnpj 
+cpf - 11 digitos
+cnpj - 14 digitos 
+*/
+
+
+
