45772c8ab63ede652f136dae3056fb49062099d5
BrunoLB2007 <brunolerin2007@gmail.com>
2024-08-27 21:47:27 -0300

    aula10.php

--- aula10.php
+++ aula10.php
@@ -27,9 +27,9 @@ function isValidCPF ($cpf){
     return false;
     }
     // Faz o calculo para validar o CPF
-    for ($t = 9; $t < 11; $t++) {
-        for ($d = 0, $c = 0; $c < $t; $c++) {
-            $d += $cpf[$c] * (($t + 1) - $c);
+    for ($i = 9; $i < 11; $i++) {
+        for ($d = 0, $c = 0; $c < $i; $c++) {
+            $d += $cpf[$c] * (($i + 1) - $c);
         }
         $d = ((10 * $d) % 11) % 10;
         if ($cpf[$c] != $d) {
@@ -43,5 +43,56 @@ function isValidCPF ($cpf){
 
 
 if (isValidCPF("793.756.240-48") == true){
-    echo "sadfsd";
+    echo "o cpf:inserido é valido";
 }
+
+
+function IsValidCNPJ ($cnpj){
+
+    $cnpj = preg_replace( '/[^0-9]/is', '', $cnpj );
+
+    if (strlen($cnpj) != 14){
+        return false;
+    }
+
+    if (preg_match('/(\d)\1{10}/', $cnpj)){
+        return false;
+    }
+
+    $b = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
+
+    for ($i = 0, $n = 0; $i < 12; $n += $cnpj[$i] * $b[++$i]);  {
+}
+ if ($cnpj[12] != ((($n %= 11) < 2) ? 0 : 11 - $n)) {
+        return false;
+    }
+    for ($i = 0, $n = 0; $i <= 12; $n += $cnpj[$i] * $b[$i++]);
+    if ($cnpj[13] != ((($n %= 11) < 2) ? 0 : 11 - $n)) {
+        return false;
+    }
+  return true;
+}
+
+
+
+function addMaskCPF ($cpf){ 
+    $cpfMascarado = ""; 
+    $maskcpf= "###.###.###-##";
+    $j = 0; 
+
+    for ($i=0; $i<= strlen($maskcpf); $i++){
+
+        if( $maskcpf[$i]== "#"){
+            $cpfMascarado .= $cpf[$j];
+            $j++;
+        }else {
+            $cpfMascarado .= $maskcpf[$i];
+        }
+    
+    }
+
+    return $cpfMascarado;
+}
+
+echo addMaskCPF("12345678910");
+
